828f3b6b98513ecd6bb4afad979fbcc3e84d8898
Shaar-games <xdiabloleo31@gmail.com>
2021-04-05 09:49:51 +0200

    Ajoutez des fichiers projet.

---
+++ test-tech/Startup.cs
@@ -0,0 +1,108 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace test_tech
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider("C:/Dev/test_tech/Test-technique/dist/css"),
+                RequestPath = "/css"
+            });
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider("C:/Dev/test_tech/Test-technique/dist/fonts"),
+                RequestPath = "/fonts"
+            });
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider("C:/Dev/test_tech/Test-technique/dist/img"),
+                RequestPath = "/img"
+            });
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider("C:/Dev/test_tech/Test-technique/dist/js"),
+                RequestPath = "/js"
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync(ReadFile("C:/Dev/test_tech/Test-technique/dist/index.html")); // ...
+
+                });
+            });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+        }
+        public static string ReadFile(string FileName)
+        {
+            try
+            {
+                using (StreamReader reader = File.OpenText(FileName))
+                {
+                    string fileContent = reader.ReadToEnd();
+                    if (fileContent != null && fileContent != "")
+                    {
+                        return fileContent;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log
+                throw ex;
+            }
+            return null;
+        }
+    }
+}
